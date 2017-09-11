b0d72560885988778639b6ae5349b25e779267c8
nerdrun <nerdrun.kai@gmail.com>
2017-09-11 18:56:21 +0900

    board template for S3 and cloudfront

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now() + '.' + file.mimetype.split('/')[1]);
+  }
+});
+
+var upload = multer({storage: storage});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
@@ -8,9 +19,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
-app.listen(80, () => {
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.post('/form', upload.single('image') ,(req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.redirect('/list');
+});
+
+app.get('/list', (req, res) => {
+  res.render('list');
+});
+
+app.listen(3000, () => {
   console.log('Server has been started');
 });
